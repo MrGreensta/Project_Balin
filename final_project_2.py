90296485ccf96f5bbaf307dbc5028d19ea1d42ab
MrGreensta <mrgreensta@gmail.com>
2023-01-14 00:33:48 +0500

    update

--- final_project_2.py
+++ final_project_2.py
@@ -1,10 +1,13 @@
+import pandas as pd
+
+
 def hrs_to_mins(time: str) -> int:
 
     '''Данная функция принимает строку, содержащую информацию о времени
     в различных форматах (количество часов или количество минут) и,
     если это необходимо, переводит значение в количество минут'''
 
-    if len(time) == 0: #Т.к. в базе есть 4 одинаковых записи без указания времени.
+    if len(time) == 0: # Т.к. в базе есть 4 одинаковых записи без указания времени.
         return None
     time_list = time.split()
     if time_list[1] == "mins":
@@ -14,7 +17,12 @@ def hrs_to_mins(time: str) -> int:
         mins += int(time_list[2])
     return mins
 
+
 assert hrs_to_mins("") == None
 assert hrs_to_mins("0 mins") == 0
 assert hrs_to_mins("10 mins") == 10
 assert hrs_to_mins("1 hrs 5 mins") == 65
+
+
+df = pd.read_csv('recipes_100.csv')
+
